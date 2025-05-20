3b9b5d35dc0c41e8c15a0774428a75df3d4f6c87
Rprog-06 <145431249+Rprog-06@users.noreply.github.com>
2025-05-20 12:38:33 +0530

    Update ViewJobs.jsx

--- client/frontend/src/pages/ViewJobs.jsx
+++ client/frontend/src/pages/ViewJobs.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-import api from "../api/axiosInstance"
+import API from "../api/axiosInstance"
 
 const ViewJobs = () => {
   const [jobs, setJobs] = useState([]);
