c39ef32b4bd9b0525d0efc6efb8bc9ce3f25ec6c
Rprog-06 <145431249+Rprog-06@users.noreply.github.com>
2025-05-20 13:09:52 +0530

    Update ApplyForm.jsx

--- client/frontend/src/components/ApplyForm.jsx
+++ client/frontend/src/components/ApplyForm.jsx
@@ -37,7 +37,7 @@ const ApplyForm = () => {
       }
       data.append("jobId", jobId);
 
-      await API.post("/applications/apply", data, {
+      await API.post("/api/applications/apply", data, {
         headers: {
           "Content-Type": "multipart/form-data",
           Authorization: `Bearer ${token}`,
