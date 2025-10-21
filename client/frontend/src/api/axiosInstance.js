bd8c68f49e9eb23304043d9d5a0affb78e4c3349
Rizwan Vahora <vahorarizwan09@gmail.com>
2025-10-22 00:26:56 +0530

    Updated

--- client/frontend/src/api/axiosInstance.js
+++ client/frontend/src/api/axiosInstance.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: process.env.REACT_APP_API_BASE_URL|| 'http://localhost:5000/api', // e.g. https://your-backend.onrender.com/api
+  baseURL: process.env.REACT_APP_API_BASE_URL|| 'http://localhost:5000/api',
+  withCredentials:true, // e.g. https://your-backend.onrender.com/api
 });
 
 export default API;
