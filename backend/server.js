18918f67dfc9937dfe6e31a53e1625ecbf142d83
Rizwan Vahora <vahorarizwan09@gmail.com>
2025-10-22 15:42:54 +0530

    Remove

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,12 @@ const app = express();
 //   credentials: true,
 // };
 
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:3000',
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
 app.use(express.json());
 app.get('/', (req, res) => {
