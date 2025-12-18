386a246165322270fe52b1476b3c102bc5df79af
Rizwan Vahora <vahorarizwan09@gmail.com>
2025-12-18 20:02:08 +0530

    P

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -1,15 +1,14 @@
 const nodemailer = require("nodemailer");
 
 const sendEmail = async (to, subject, text,attachmentPath) => {
-  const transporter = nodemailer.createTransport({
-    
-    host: "smtp.gmail.com",
+  const transporter = nodemailer.createTransport({  
+   host: "smtp-relay.brevo.com",
   port: 587,
   secure: false,
     auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
-    },
+    user: process.env.BREVO_SMTP_USER,
+    pass: process.env.BREVO_SMTP_PASS,
+  },
      tls: {
     rejectUnauthorized: false,
   },
